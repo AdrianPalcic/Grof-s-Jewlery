46a6e36eb80b07fc67ba36f84d6404db5cd5014c
Adrian Palcic <adrian.palcic@gmail.com>
2025-09-11 15:12:17 +0200

    turned it into a flex container

--- app/kategorije/components/SubcategorySection.tsx
+++ app/kategorije/components/SubcategorySection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import ButtonMain from "@/app/compontents/ButtonMain";
 import GhostButton from "@/app/compontents/GhostButton";
 import Link from "next/link";
@@ -36,29 +38,34 @@ const SubcategorySection = ({ name }: { name: string }) => {
   ];
 
   return (
-    <section className=" flex flex-col mb-6">
-      <h1 className="text-2xl sm:text-4xl mb-2">{name}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-12 gap-x-4 mb-5">
+    <section className="flex flex-col mb-6">
+      <h1 className="text-2xl sm:text-4xl mb-4">{name}</h1>
+
+      <div className="flex gap-4 overflow-x-auto scrollbar-hide snap-x snap-mandatory mb-5">
         {items.map((item) => (
-          <div key={item.id}>
+          <div
+            key={item.id}
+            className="flex-none w-full smm:w-[300px] snap-start"
+          >
             <Link
               href={`/kategorije/${item.itemCat}/${name}/${item.title}`}
-              className="overflow-hidden"
+              className="overflow-hidden block"
             >
               <img
                 src={item.img}
                 alt={item.title}
-                className="w-full min-h-[250px] h-[300px] object-cover transition-opacity duration-300 hover:opacity-70"
+                className="w-full h-[300px] object-cover transition-opacity duration-300 hover:opacity-70"
               />
             </Link>
-            <div className="flex flex-col items-start justify-start w-full">
-              <h2 className="mt-2 text-xl font-semibold">{item.title}</h2>
+            <div className="flex flex-col items-start justify-start w-full mt-2">
+              <h2 className="text-xl font-semibold">{item.title}</h2>
               <p className="mb-2">{item.price}</p>
               <GhostButton text="Dodaj u košaricu" />
             </div>
           </div>
         ))}
       </div>
+
       <Link href={`/kategorije/${items[0].itemCat}/${name}`}>
         <ButtonMain text="Svi Artikli" />
       </Link>
