e2c606b90f1416e293708cbe2a889a02980b64d2
Adrian Palcic <adrian.palcic@gmail.com>
2025-09-12 15:14:30 +0200

    Added about page

--- app/onama/page.tsx
+++ app/onama/page.tsx
@@ -1,7 +1,54 @@
 import React from "react";
+import CTA from "../compontents/CTA";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className="px-4 sm:px-10 mx-auto mt-10 mb-20 ">
+      <h1 className="text-3xl sm:text-5xl">O nama</h1>
+      <div className="w-full flex items-center justify-center min-h-[300px] h-[300px] sm:h-[500px] mt-5">
+        <img
+          src="/kontakt.png"
+          alt="Ručno rađeni papirnati nakit"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-full my-5">
+        <p>
+          {" "}
+          Više od tri desetljeća, Grof’s Jewelry simbol je vrhunske izrade i
+          bezvremenskog dizajna. Svaki komad pomno je ručno izrađen od strane
+          naših majstora obrtnika, koristeći samo najfinije materijale. Naša
+          predanost izvrsnosti nadilazi samu estetiku – obuhvaća i etičko
+          podrijetlo te održivu praksu, osiguravajući da svaki komad ne samo da
+          ukrašava, već i priča priču o vještini, tradiciji i poštovanju prema
+          materijalu. <br /> <br /> Naša predanost izvrsnosti nadilazi samu
+          estetiku – obuhvaća i etičko podrijetlo te održivu praksu,
+          osiguravajući da svaki komad ne samo da ukrašava, već i priča priču o
+          vještini, tradiciji i poštovanju prema materijalu. Više od tri
+          desetljeća, Grof’s Jewelry simbol je vrhunske izrade i bezvremenskog
+          dizajna. Svaki komad pomno je ručno izrađen od strane naših majstora
+          obrtnika, koristeći samo najfinije materijale. Naša predanost
+          izvrsnosti nadilazi samu estetiku – obuhvaća i etičko podrijetlo te
+          održivu praksu, osiguravajući da svaki komad ne samo da ukrašava, već
+          i priča priču o vještini, tradiciji i poštovanju prema materijalu.{" "}
+          <br /> <br /> Naša predanost izvrsnosti nadilazi samu estetiku –
+          obuhvaća i etičko podrijetlo te održivu praksu, osiguravajući da svaki
+          komad ne samo da ukrašava, već i priča priču o vještini, tradiciji i
+          poštovanju prema materijalu. Više od tri desetljeća, Grof’s Jewelry
+          simbol je vrhunske izrade i bezvremenskog dizajna. Svaki komad pomno
+          je ručno izrađen od strane naših majstora obrtnika, koristeći samo
+          najfinije materijale. Naša predanost izvrsnosti nadilazi samu estetiku
+          – obuhvaća i etičko podrijetlo te održivu praksu, osiguravajući da
+          svaki komad ne samo da ukrašava, već i priča priču o vještini,
+          tradiciji i poštovanju prema materijalu. <br /> <br /> Naša predanost
+          izvrsnosti nadilazi samu estetiku – obuhvaća i etičko podrijetlo te
+          održivu praksu, osiguravajući da svaki komad ne samo da ukrašava, već
+          i priča priču o vještini, tradiciji i poštovanju prema materijalu.
+        </p>
+      </div>
+      <CTA text="kontaktirajte nas!" link="kontakt" />
+    </main>
+  );
 };
 
 export default page;
