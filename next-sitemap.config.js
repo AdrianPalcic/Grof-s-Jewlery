b86fd2b2aca1cfca9ded952c50e58967da6ca96b
Adrian Palcic <adrian.palcic@gmail.com>
2025-12-04 16:55:03 +0100

    back to basic

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
-const config = {
+module.exports = {
   siteUrl: process.env.SITE_URL || "https://grofsjewelry.com",
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  changefreq: "daily", // note: small c
-  priority: 0.7,
+  changeFreq: "daily",
+  prority: 0.7,
 };
-
-export default config;
