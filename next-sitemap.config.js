cc43d3a00b683bbe9e1b103919aa6bbe8a7a1e8a
Adrian Palcic <adrian.palcic@gmail.com>
2025-12-04 15:44:28 +0100

    tried to update my sitemap - will do next time

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://grofsjewelry.com",
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  changeFreq: "daily",
-  prority: 0.7,
-};
+export const siteUrl = process.env.SITE_URL || "https://grofsjewelry.com";
+export const generateRobotsTxt = true;
+export const sitemapSize = 5000;
+export const changeFreq = "daily";
+export const prority = 0.7;
