c1778980d2a8fe0f465d02dd20efa229d07f1bca
Adrian Palcic <adrian.palcic@gmail.com>
2025-12-04 17:12:41 +0100

    coderabbit help

--- app/proizvodi/[slug]/page.tsx
+++ app/proizvodi/[slug]/page.tsx
@@ -3,13 +3,15 @@ import AddToCartBtn from "@/app/compontents/AddToCartBtn";
 import AddToGiftBoxBtn from "@/app/compontents/AddToGiftBoxBtn";
 import ItemImages from "@/app/compontents/ItemImages";
 
-import React from "react";
 import { Metadata } from "next";
 
 type PageProps = {
-  params: { slug: string };
+  params: {
+    slug: string;
+  };
 };
 
+// Metadata za SEO i OG
 export async function generateMetadata({
   params,
 }: PageProps): Promise<Metadata> {
@@ -25,7 +27,7 @@ export async function generateMetadata({
       description:
         product.description || "Pregledajte naš unikatni nakit i poklone.",
       url: `https://grofsjewelry.com/proizvodi/${params.slug}`,
-      siteName: "Grof's Jewlery",
+      siteName: "Grof's Jewelry",
       images: [
         {
           url: ogImage,
@@ -46,6 +48,7 @@ export async function generateMetadata({
   };
 }
 
+// Page komponenta
 export default async function Page({ params }: PageProps) {
   const product = await getProductByHandle(params.slug);
   const images = product.images.edges.map((edge) => edge.node);
@@ -53,6 +56,7 @@ export default async function Page({ params }: PageProps) {
   return (
     <section className="px-4 sm:px-10 mx-auto mt-10 mb-20">
       <div className="flex flex-col-reverse lg:flex-row-reverse px-2 sm:px-10 items-center gap-2 lg:gap-10 mx-auto mb-20 py-10">
+        {/* Info */}
         <div className="flex-[0.6]">
           <h2 className="text-3xl">{product.title}</h2>
           <p className="text-2xl font-cormorant">
@@ -61,19 +65,22 @@ export default async function Page({ params }: PageProps) {
           <div className="w-full h-[1px] bg-secondaryColor mt-2"></div>
 
           <p className="text-xl smm:w-[90%] mt-2 mb-4">{product.description}</p>
+
           {product.availableForSale ? (
             <div className="flex flex-col sm:flex-row w-full items-center justify-between gap-3 mb-5">
               <AddToCartBtn product={product} />
               <AddToGiftBoxBtn product={product} />
             </div>
           ) : (
-            <button className="btn w-full opacity-50 mb-2" disabled={true}>
+            <button className="btn w-full opacity-50 mb-2" disabled>
               Rasprodano
             </button>
           )}
+
           <div className="w-full h-[1px] bg-secondaryColor mb-2"></div>
         </div>
 
+        {/* Slike */}
         <ItemImages images={images} />
       </div>
     </section>
